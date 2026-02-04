4e80487fc09ce7b8dbead952e8e72add3d991af7
VincentsSin <vincentssin@gmail.com>
2026-02-04 03:20:48 -0600

    Update application details in resource header

--- dllmain/resource.h
+++ dllmain/resource.h
@@ -2,16 +2,16 @@
 
 // Used by dllmain.rc
 
-#define APP_NAME				"re4_tweaks"
+#define APP_NAME				"re4_ap_mod"
 #define APP_MAJOR				1
-#define APP_MINOR				9
-#define APP_BUILDNUMBER			1
+#define APP_MINOR				0
+#define APP_BUILDNUMBER			0
 #define APP_REVISION			0
-#define APP_COMPANYNAME			"nipkow"
-#define APP_DESCRPTION			"Fixes and tweaks for the UHD port of Resident Evil 4"
-#define APP_COPYRIGHT			"Copyright (C) 2021-2023 nipkow"
+#define APP_COMPANYNAME			"VincentsSin"
+#define APP_DESCRPTION			"Archipelago mod for the UHD port of Resident Evil 4"
+#define APP_COPYRIGHT			"Copyright (C) 2026 VincentsSin"
 #define APP_ORIGINALVERSION		"dinput8.dll"
-#define APP_INTERNALNAME		"re4_tweaks"
+#define APP_INTERNALNAME		"re4_ap_mod"
 
 // Get APP_VERSION
 #define _TO_STRING_(x) #x
