1896ef3f602c78769639c8c7f3479964f97484a7
VincentsSin <vincentssin@gmail.com>
2026-02-04 02:47:53 -0600

    Refactor logging and initialization for re4_ap_mod

--- dllmain/dllmain.cpp
+++ dllmain/dllmain.cpp
@@ -34,7 +34,7 @@ void Init_Main()
 	std::string commit = GIT_CUR_COMMIT;
 	commit.resize(8);
 
-	spd::log()->info("Starting re4_tweaks v{0}-{1}-{2}", APP_VERSION, commit, GIT_BRANCH);
+	spd::log()->info("Starting re4_ap_mod v{1}-{0}-{0}", APP_VERSION, commit, GIT_BRANCH);
 	spd::LogProcessNameAndPID();
 	spd::log()->info("Running from: \"{}\"", WstrToStr(rootPath));
 	spd::log()->info("Game version: {}", GameVersion());
@@ -52,67 +52,15 @@ void Init_Main()
 
 		spd::log()->info("RE4 HD Project detected");
 
-		re4t::cfg->bIsUsingHDProject = true;
-		re4t::init::HDProject();
+		re4ap::cfg->bIsUsingHDProject = true;
+		re4ap::init::HDProject();
 	}
 
 	// Init input-related hooks and populate keymap (needed before we ReadSettings(), otherwise, parsing hotkeys will fail)
 	pInput->init();
 
 	// Read re4_tweaks settings
-	re4t::cfg->ReadSettings();
-
-	// Log re4_tweaks settings
-	re4t::cfg->LogSettings();
-
-	// Parse any special command-line options
-	re4t::init::CommandLine();
-
-	// Init game/re4t log window
-	re4t::init::ConsoleWnd();
-
-	// Check for re4_tweaks updates
-	CreateThreadAutoClose(0, 0, (LPTHREAD_START_ROUTINE)&updateCheck, NULL, 0, NULL);
-
-	// Various display-related tweaks
-	re4t::init::DisplayTweaks();
-
-	re4t::init::AudioTweaks();
-
-	re4t::init::CameraTweaks();
-
-	re4t::init::KeyboardMouseTweaks();
-
-	re4t::init::ControllerTweaks();
-
-	// Install a WndProc hook and register the resulting hWnd for input
-	re4t::init::WndProcHook();
-
-	// Increase some game limits
-	re4t::init::HandleLimits();
-
-	// QTE-related changes
-	re4t::init::QTEfixes();
-
-	// Fixes FPS-related issues
-	re4t::init::FrameRateFixes();
-
-	re4t::init::HUDTweaks();
-
-	re4t::init::Gameplay();
-
-	re4t::init::Misc();
-
-	if (re4t::cfg->bEnableModExpansion)
-		re4t::init::ModExpansion();
-
-	re4t::init::MathReimpl();
-
-	re4t::init::DebugDisplay();
-
-	re4t::init::TitleMenu();
-
-	Trainer_Init();
+	re4ap::cfg->ReadConnection();
 }
 
 // Dll main function
